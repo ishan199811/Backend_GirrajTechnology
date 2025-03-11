80fd02d410bcfb3df24fd69e90e0f3f035afea4f
ishan199811 <ishansharma199811@gmail.com>
2025-03-11 17:01:46 +0530

    update contact

--- GirrajTechnology/src/main/java/com/GirrajTechnology/model/Contact.java
+++ GirrajTechnology/src/main/java/com/GirrajTechnology/model/Contact.java
@@ -17,56 +17,35 @@ public class Contact {
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long contactId;
 
+	String fullName;
 	
-	String firstName;
-	String message;
+	String email;
+	
 	String mobile_no;
+
+	String message;
+	
 	public String getMobile_no() {
 		return mobile_no;
 	}
 	public void setMobile_no(String mobile_no) {
 		this.mobile_no = mobile_no;
 	}
-	String lastName;
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	String name;
-	String email;
-	String project;
+	
 	public Long getContactId() {
 		return contactId;
 	}
 	public void setContactId(Long contactId) {
 		this.contactId = contactId;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getProject() {
-		return project;
-	}
-	public void setProject(String project) {
-		this.project = project;
-	}
+	
 	public String getMessage() {
 		return message;
 	}
